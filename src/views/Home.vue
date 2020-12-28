ac6a72234bd5a7064e2aaccb23e5901ebc01fdd7
Adam Kalimi <adam.kalimi@yahoo.com>
2020-12-28 15:11:50 +0800

    Added data db locally. Created SingleProject.vue component. Moved data for projects over to the SingleProject component using props.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="home">
     <p>Home</p>
+    <div v-if="projects.length">
+      <!-- cycle throughthe projects -->
+      <div v-for="project in projects" :key="project.id">
+        <!-- saving the value of the projects above to the prop :projects -->
+        <SingleProject :project="project"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import SingleProject from '../components/SingleProject.vue'
 export default {
   name: 'Home',
-  components: { }
+  data() {
+    return {
+      projects: [] 
+    }
+  },
+  components: { SingleProject },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+    //gets the response and parses the json
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
+  }
 }
 </script>
